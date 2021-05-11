514391919b826083d19e43c8dea80835a6636d89
Antonio <antonio.alexander.a10@gmail.com>
2021-05-10 23:50:09 -0400

    Validacion de texto y cambio de fuente

--- js/javascript.js
+++ js/javascript.js
@@ -3,115 +3,118 @@ const text = document.querySelector('#text');
 const srtText = text.textContent;
 const splitText = srtText.split("");
 text.textContent = "";
-for (let i =0 ; i <  srtText.length; i++ ) {
-text.innerHTML += "<span>" + splitText[i] + "</span>";  
+
+for (let i = 0; i < srtText.length; i++) {
+  text.innerHTML += "<span>" + splitText[i] + "</span>";
 }
 
 let char = 0;
-let timer = setInterval(onTick,50);
+let timer = setInterval(onTick, 50);
 
-function onTick(){
+function onTick() {
   const span = text.querySelectorAll('span')[char];
   span.classList.add('faded');
   char++
-  if(char === splitText.length){
+  if (char === splitText.length) {
     complete();
     return;
   }
 }
 
-function complete(){
+function complete() {
   clearInterval(timer);
   timer = null;
 }
 
+let boton = document.getElementById('btn');
 
+boton.addEventListener('click', () => {
 
-let boton =document.getElementById('btn');
+  // 1º vamos a capturar el valor del input
+  let box = document.getElementById('box').value
 
-boton.addEventListener('click',()=>{
+  // 2º vamos a tomar la imagen y texto de informacion del pokemon
+  let img = document.getElementById('imagen')
 
-   // 1º vamos a capturar el valor del input
-    let box=document.getElementById('box').value
+  // 3º vamos a traenos la info (nombre)
+  let p = document.getElementById('info')
 
-    // 2º vamos a tomar la imagen y texto de informacion del pokemon
-    let img=document.getElementById('imagen')
+  // 4º saber la habilidad
+  let span = document.getElementById('habilidad')
 
-    // 3º vamos a traenos la info (nombre)
-    let p=document.getElementById('info')
+  // 5º saber la estadísticas
+  // let cont=document.getElementById('chartContainer')
 
-    // 4º saber la habilidad
-    let span=document.getElementById('habilidad')
+  const estadisticas = [];
 
-    // 5º saber la estadísticas
-    // let cont=document.getElementById('chartContainer')
+  // vamos hacer el llamado a la API
+  let xhttp = new XMLHttpRequest()
 
-    const estadisticas = [];
+  if (isNaN(box)) {
+    alert("Valor invalido, ingrese un numero");
+  }
+  
+  if (!isNaN(box)) {
+    xhttp.open("GET", `https://pokeapi.co/api/v2/pokemon/${box}`)
+    xhttp.send()   
+  }
 
-    // vamos hacer el llamado a la API
-    let xhttp =new XMLHttpRequest()
-    xhttp.open("GET",`https://pokeapi.co/api/v2/pokemon/${box}`)
-    xhttp.send()
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
 
-    xhttp.onreadystatechange=function() {
-        if(this.readyState== 4 && this.status==200){
+      let datePokemon = JSON.parse(this.responseText)
+      img.setAttribute("src", datePokemon.sprites.front_default)
+      p.textContent = datePokemon.name
+      span.textContent = datePokemon.id
 
-            let datePokemon=JSON.parse(this.responseText)
-            img.setAttribute("src",datePokemon.sprites.front_default)
-            p.textContent=datePokemon.name
-            span.textContent=datePokemon.id
+      statsLength = (datePokemon.stats).length;
 
-            statsLength = (datePokemon.stats).length;
+      for (var i = 0; i < statsLength; i++) {
+        estadisticas.push({
+          y: parseInt(datePokemon.stats[i].base_stat),
+          label: (datePokemon.stats[i].stat.name)
+        });
+      }
 
-            for (var i = 0; i < statsLength; i++) {
-              estadisticas.push({
-                y: parseInt(datePokemon.stats[i].base_stat),
-                label: (datePokemon.stats[i].stat.name)
-              });
-            }
-
-          chart.render();
-
-        }
+      chart.render();
 
     }
 
-    // console.log(estadisticas);
+  }
 
-    /// testing general graficos
-    var chart = new CanvasJS.Chart("chartContainer",
+  var chart = new CanvasJS.Chart("chartContainer",
+    {
+      backgroundColor: "#FFCC00",
+      animationEnabled: true,
+      animationDuration: 1800,
+
+      title: {
+        text: "Estadísticas Base",
+        fontFamily: "Impact",
+        fontWeight: "normal"
+      },
+
+      legend: {
+        verticalAlign: "bottom",
+        horizontalAlign: "center"
+      },
+      data: [
         {
-            backgroundColor: "#FFCC00",
-            animationEnabled: true,
-            animationDuration: 1800,
+          startAngle: 55,
+          indexLabelFontSize: 20,
+          indexLabel: "{label} - #percent%",
+          indexLabelFontFamily: "Garamond",
+          indexLabelFontColor: "white",
+          indexLabelLineColor: "white",
+          indexLabelPlacement: "outside",
+          toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+          type: "doughnut",
+          // showInLegend: true,
+          dataPoints: estadisticas
+        }
+      ]
+    });
 
-          title:{
-            text: "Estadísticas Base",
-            fontFamily: "Impact",
-            fontWeight: "normal"
-          },
-          
-          legend:{
-            verticalAlign: "bottom",
-            horizontalAlign: "center"
-          },
-          data: [
-          {
-           startAngle: 55,
-           indexLabelFontSize: 20,
-           indexLabel: "{label} - #percent%",
-           indexLabelFontFamily: "Garamond",
-           indexLabelFontColor: "white",
-           indexLabelLineColor: "white",
-           indexLabelPlacement: "outside",
-           toolTipContent: "<b>{label}:</b> {y} (#percent%)",
-           type: "doughnut",
-           // showInLegend: true,
-           dataPoints: estadisticas
-         }
-         ]
-       });
+  // chart.render();
 
-       // chart.render();
-
-  })
+})
